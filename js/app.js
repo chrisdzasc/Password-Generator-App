76ee8df49f34302d0a656c6451784a79e37de7fc
Christian Diaz <christian.dzasc@gmail.com>
2026-04-07 13:05:44 -0600

    feat: set up DOM references and generate button event listener
    - Select character length, checkbox input, and generate button
    - Add click event listener to generate button

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const characterLengthValue = document.querySelector("#characterLength");
+const uppercaseLettersCheckbox = document.querySelector("#uppercaseLetters");
+const lowercaseLettersCheckbox = document.querySelector("#lowercaseLetters");
+const numbersCheckbox = document.querySelector("#numbers");
+const symbolsCheckbox = document.querySelector("#symbols");
+
+const generatePasswordButton = document.querySelector("#generatePassword");
+
+function generatePassword() {
+
+}
+
+
+generatePasswordButton.addEventListener("click", generatePassword);
